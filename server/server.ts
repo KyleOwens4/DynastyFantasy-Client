417d5c557842bf97293e6ba19d8bbf438e753f65
KyleOwens4 <owens.w.kyle@gmail.com>
2024-03-26 20:53:17 -0500

    Update to express

---
+++ server/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from express test TSa!!!");
+});
+
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+  console.log("Press Ctrl-C to exit");
+});
