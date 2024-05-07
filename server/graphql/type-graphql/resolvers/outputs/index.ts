c94f3f0bfec6c18c48d255b3fbb28bad5978d208
Kyle <kwowens4@wisc.edu>
2024-05-06 20:38:00 -0500

    Removing all the generated stuff

--- server/graphql/type-graphql/resolvers/outputs/index.ts
+++
@@ -1,6 +0,0 @@
-export { AffectedRowsOutput } from "./AffectedRowsOutput";
-export { AggregateUsers } from "./AggregateUsers";
-export { UsersCountAggregate } from "./UsersCountAggregate";
-export { UsersGroupBy } from "./UsersGroupBy";
-export { UsersMaxAggregate } from "./UsersMaxAggregate";
-export { UsersMinAggregate } from "./UsersMinAggregate";
