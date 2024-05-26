0e3d62f17a5d4919f043c34dc4965112b22a69fd
Kyle <kwowens4@wisc.edu>
2024-05-26 18:19:28 -0500

    Updated node resolver

--- server/resolvers/NodeResolver.ts
+++ server/resolvers/NodeResolver.ts
@@ -1,19 +1,16 @@
 import { Arg, ID, Query } from "type-graphql";
 import { ParseUUID } from "../graphql/utils/RelayUUIDs";
-import { PrismaClient } from "@prisma/client";
 import { UserResolver } from "./UserResolver";
 import { Node } from "../graphql/interfaces/Node";
-import { User } from "../graphql/types/User";
-
-const prisma = new PrismaClient
 
 export class NodeResolver {
     @Query((_returns) => Node, { nullable: true })
     async node(@Arg("id", (_type) => ID!) id: string) {
         const { typeName } = ParseUUID(id);
 
-        if (typeName === "User") {
-            return new UserResolver().getByID(id)
+        switch(typeName) {
+            case "User":
+                return new UserResolver().getByID(id);
         }
 
         return null;
