43a5ff35850430733c140f51adb99f123057dff6
KyleOwens4 <owens.w.kyle@gmail.com>
2024-06-29 17:03:42 -0500

    fix environment variable naming

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 console.log(import.meta.env);
 export const SUPABASE_URL =
-  import.meta.env.SUPABSE_URL ?? "http://127.0.0.1:54321";
+  import.meta.env.SUPABASE_URL ?? "http://127.0.0.1:54321";
 export const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY ?? "FAIL";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
