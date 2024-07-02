cdddc3377ec26e09cd22a49bc94127d4f98838c3
KyleOwens4 <owens.w.kyle@gmail.com>
2024-07-01 21:37:49 -0500

    Mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { graphql } from "relay-runtime";
 import { useLazyLoadQuery } from "react-relay";
 import { AppQuery as AppQueryType } from "../data/__generated__/AppQuery.graphql";
+import { AppShell, Card } from "@mantine/core";
+import { Bird } from "lucide-react";
 
 const AppQuery = graphql`
   query AppQuery {
@@ -18,11 +20,22 @@ const AppQuery = graphql`
 
 function App() {
   const data = useLazyLoadQuery<AppQueryType>(AppQuery, {});
-
   return (
-    <div className="bg-red-500 text-white text-sm">
-      {data.profilesCollection?.edges[0].node.nodeId}
-    </div>
+    <AppShell header={{ height: 50 }} padding="md">
+      <AppShell.Header>
+        <div className="flex h-full items-center space-x-4 px-8">
+          <Bird />
+          <p className="font-bold">Dynasty Fantasy</p>
+        </div>
+      </AppShell.Header>
+
+      <AppShell.Main className="flex items-center justify-center">
+        <Card className="w-[75%] h-fit" shadow="lg">
+          <h1 className="text-xl font-bold">Sign Up</h1>
+          <p>use this section to login</p>
+        </Card>
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RelayEnvironmentProvider environment={environment}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <MantineProvider
+      theme={{
+        fontFamily: "Inter var, " + defaultTheme.fontFamily.sans.join(", "),
+      }}
+    >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </MantineProvider>
   </RelayEnvironmentProvider>
 );
